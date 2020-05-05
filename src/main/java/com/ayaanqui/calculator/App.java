af41b10d6e7fa7415b55bac92f91139c84b6d6cc
ayaanqui <moahammedayaan.dev@gmail.com>
2020-05-05 00:02:38 -0500

    Renamed class to App

--- src/main/java/com/ayaanqui/calculator/App.java
+++ src/main/java/com/ayaanqui/calculator/App.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 import java.io.File;
 import java.io.IOException;
 
-public class Run {
+public class App {
     public static void main(String[] args) throws IOException {
         Scanner keyboard = new Scanner(System.in);
         Calculator calc = new Calculator();
