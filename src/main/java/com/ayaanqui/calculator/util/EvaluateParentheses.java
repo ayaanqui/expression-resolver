28d0f45bc297caec68805736e0ff5651acca1627
ayaanqui <moahammedayaan.dev@gmail.com>
2020-05-06 22:01:51 -0500

    New static condense function

--- src/main/java/com/ayaanqui/calculator/util/EvaluateParentheses.java
+++ src/main/java/com/ayaanqui/calculator/util/EvaluateParentheses.java
@@ -7,44 +7,30 @@ import com.ayaanqui.calculator.algorithms.RelatedParentheses;
 import com.ayaanqui.calculator.Calculator;
 
 public class EvaluateParentheses {
-    private ArrayList<String> formattedUserInput;
-    private String solvedInnerExpression;
-
     public EvaluateParentheses() {
     }
 
-    public EvaluateParentheses(ArrayList<String> formattedUserInput) {
-        this.formattedUserInput = formattedUserInput;
-    }
-
-    public void condense(int start) {
-        Map<Integer, Integer> relatedParentheses = new RelatedParentheses(formattedUserInput).evaluateRelations();
+    public static String condense(ArrayList<String> formattedList, int start) {
+        Map<Integer, Integer> relatedParentheses = new RelatedParentheses(formattedList).evaluateRelations();
 
         int end = relatedParentheses.get(start);
 
         String innerExpression = "";
 
         // Populate innerExpression with all of the elemnts inside the parentheses
-        for (int t = start + 1; t < end; t++) {
-            innerExpression += formattedUserInput.get(t);
-        }
+        for (int t = start + 1; t < end; t++)
+            innerExpression += formattedList.get(t);
+
+        // Removing elements from the back, to avoid IndexOutOfBounds Errors
+        for (int t = end; t > start; t--)
+            formattedList.remove(t);
 
         Calculator newExpression = new Calculator();
         newExpression.expression(innerExpression);
-        solvedInnerExpression = newExpression.solveExpression() + "";
+        String solvedInnerExpression = newExpression.solveExpression();
 
-        // Removing elements from the back, to avoid IndexOutOfBounds Errors
-        for (int t = end; t > start; t--) {
-            formattedUserInput.remove(t);
-        }
-        formattedUserInput.set(start, solvedInnerExpression);
-    }
+        formattedList.set(start, solvedInnerExpression);
 
-    public ArrayList<String> getFormattedUserInput() {
-        return formattedUserInput;
-    }
-
-    public String getSolvedInnerExpression() {
         return solvedInnerExpression;
     }
 }
