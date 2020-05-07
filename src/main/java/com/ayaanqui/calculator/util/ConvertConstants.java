2e9e8ed676714adf1f6090de43ad90a93c71298d
ayaanqui <moahammedayaan.dev@gmail.com>
2020-05-06 19:22:11 -0500

    Removed redundant return and re-organized loop

--- src/main/java/com/ayaanqui/calculator/util/ConvertConstants.java
+++ src/main/java/com/ayaanqui/calculator/util/ConvertConstants.java
@@ -1,12 +1,11 @@
 package com.ayaanqui.calculator.util;
 
-import java.util.Map;
 import java.util.HashMap;
 import java.util.ArrayList;
 
 public class ConvertConstants {
     final private String[] constants = { "pi", "e", "tau" };
-    private Map<String, Double> constantMap = new HashMap<String, Double>();
+    private HashMap<String, Double> constantMap = new HashMap<>();
     private ArrayList<String> userInpList;
 
     public ConvertConstants(ArrayList<String> userInpList) {
@@ -18,18 +17,14 @@ public class ConvertConstants {
         constantMap.put("tau", 2 * Math.PI);
     }
 
-    public ArrayList<String> convert() {
-        for (String constant : constants) {
-            for (int i = 0; i < userInpList.size(); i++) {
-                if (userInpList.get(i).equals(constant)) {
+    public void convert() {
+        for (int i = 0; i < userInpList.size(); i++) {
+            for (String constant : constants) {
+                if (userInpList.get(i).equals(constant))
                     userInpList.set(i, constantMap.get(constant) + "");
-                } else if (userInpList.get(i).equals("-" + constant)) {
-                    // Deals with negative constants
+                else if (userInpList.get(i).equals("-" + constant)) // Deals with negative constants
                     userInpList.set(i, "-" + constantMap.get(constant));
-                }
             }
         }
-
-        return userInpList;
     }
 }
