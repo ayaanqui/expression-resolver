550e5108b93a4def8365dc2368708fe2360e37a8
ayaanqui <moahammedayaan.dev@gmail.com>
2020-05-08 21:54:02 -0500

    Removed help command

--- src/main/java/com/ayaanqui/calculator/App.java
+++ src/main/java/com/ayaanqui/calculator/App.java
@@ -1,64 +1,46 @@
 package com.ayaanqui.calculator;
 
+import java.util.ArrayList;
 import java.util.Scanner;
-import java.io.File;
 import java.io.IOException;
 
 public class App {
     public static void main(String[] args) throws IOException {
         Scanner keyboard = new Scanner(System.in);
         Calculator calc = new Calculator();
-        String userInput = "";
-        int cnt = 0;
+        String userInput;
 
-        System.out.print(">> ");
-        userInput = keyboard.nextLine();
-        userInput = userInput.toLowerCase();
+        while (true) {
 
-        while (!userInput.equals("exit") && !userInput.equals("quit")) {
-            // User help
-            if (userInput.toLowerCase().equals("help") || userInput.equals("?")) {
+            System.out.print(">> ");
+            userInput = keyboard.nextLine();
+            userInput = userInput.toLowerCase();
 
-                Scanner help = new Scanner(new File("help.txt"));
-                String helpItem = "";
-                System.out.print("\n");
+            if (userInput.equals("exit") || userInput.equals("quit"))
+                break;
 
-                while (help.hasNext()) {
-                    System.out.println(helpItem);
-                    helpItem = help.nextLine();
-                }
-                System.out.print("\n");
-                help.close();
-
-            } else if (userInput.toLowerCase().equals("history")) {
-
-                if (cnt > 0) {
-                    System.out.println("\nHISTORY:");
-                    for (int i = 0; i <= calc.getUserHistory().size() - 1; i++) {
-                        System.out.println((i + 1) + " - " + calc.getUserHistory().get(i));
-                    }
-                    System.out.println();
+            if (userInput.toLowerCase().equals("history")) {
+                ArrayList<String> history = calc.getUserHistory();
+                if (!history.isEmpty()) {
+                    for (String item : history)
+                        System.out.println(" - " + item);
                 } else {
                     System.out.println("Your history is empty :(");
                 }
-
             } else {
                 calc.expression(userInput);
                 Calculator.Response res = calc.solveExpression();
 
                 if (res.success)
-                    System.out.println(res.result + "\n");
+                    System.out.println(res.result);
                 else {
-                    for (String error : res.errors) {
+                    for (String error : res.errors)
                         System.err.println(" *" + error);
-                    }
-                    System.out.println();
                 }
             }
-            cnt++;
 
-            System.out.print(">> ");
-            userInput = keyboard.next();
+            System.out.println();
+
         }
         keyboard.close();
     }
