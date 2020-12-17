53e6de7c6f389296c14208c9b7658a8eba75d110
Ayaan Siddiqui <moahammedayaan.dev@gmail.com>
2020-12-16 18:07:24 -0600

    Use lambda forEach to iterate over constantMap

--- src/main/java/com/github/ayaanqui/ExpressionResolver/util/ConvertConstants.java
+++ src/main/java/com/github/ayaanqui/ExpressionResolver/util/ConvertConstants.java
@@ -14,12 +14,13 @@ public class ConvertConstants {
 
     public void convert() {
         for (int i = 0; i < userInpList.size(); i++) {
-            for (Map.Entry<String, Double> constant : constantMap.entrySet()) {
-                if (userInpList.get(i).equals(constant.getKey()))
-                    userInpList.set(i, constant.getValue().toString());
-                else if (userInpList.get(i).equals("-" + constant.getKey())) // Deals with negative constants
-                    userInpList.set(i, "-" + constant.getValue());
-            }
+            final int iC = i;
+            constantMap.forEach((key, value) -> {
+                if (userInpList.get(iC).equals(key))
+                    userInpList.set(iC, value.toString());
+                else if (userInpList.get(iC).equals("-" + key)) // Deals with negative constants
+                    userInpList.set(iC, "-" + value);
+            });
         }
     }
 }
